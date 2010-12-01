6fd6fb9a994b481ac1831ba7190ca8b4fcb71309
Jarad DeLorenzo <jaradd@gmail.com>
2010-12-01 10:33:16 -0500

    Added debug view

--- src/epc/labs/guesstures/Main.java
+++ src/epc/labs/guesstures/Main.java
@@ -4,36 +4,56 @@ import java.util.ArrayList;
 
 import android.app.Activity;
 import android.gesture.Gesture;
+import android.gesture.GestureLibraries;
 import android.gesture.GestureLibrary;
 import android.gesture.GestureOverlayView;
 import android.gesture.Prediction;
 import android.gesture.GestureOverlayView.OnGesturePerformedListener;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 public class Main extends Activity implements OnGesturePerformedListener {
-		private static final String TAG = "Guesstures";
-		GestureLibrary mLibrary;
-		
-		@Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+	private static final String TAG = "Guesstures";
+	GestureLibrary mLibrary;
+	
+	@Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+    mLibrary = GestureLibraries.fromRawResource(this, R.raw.gestures);
+    if(!mLibrary.load()) {
+    	finish();
     }
+    GestureOverlayView overlay = (GestureOverlayView) findViewById(R.id.gestureOverlay);
+    overlay.addOnGesturePerformedListener(this);
+  }
 
-		@Override
-		public void onGesturePerformed(GestureOverlayView overlay, Gesture gesture) {
-			ArrayList<Prediction> predictions = mLibrary.recognize(gesture);
-			if(predictions.size() > 0) {
-				Prediction prediction = predictions.get(0);
-				if(prediction.score > 1.0) {
-					onMatchFound(overlay, gesture, prediction);
-				}
+	@Override
+	public void onGesturePerformed(GestureOverlayView overlay, Gesture gesture) {
+		ArrayList<Prediction> predictions = mLibrary.recognize(gesture);
+		if(predictions.size() > 0) {
+			printGestureDebugInfo(overlay, gesture, predictions);
+			Prediction prediction = predictions.get(0);
+			if(prediction.score > 1.0) {
+				onMatchFound(overlay, gesture, prediction);
 			}
 		}
+	}
 
-		private void onMatchFound(GestureOverlayView overlay, Gesture gesture, Prediction prediction) {
-			Log.i(TAG, "Match Found!: "+prediction.name);
-			
+	private void printGestureDebugInfo(GestureOverlayView overlay,
+		Gesture gesture, ArrayList<Prediction> predictions) {
+		TextView tv = (TextView) findViewById(R.id.result);
+		tv.setText("DEBUG:\r\n");
+		tv.append("Num Strokes: " + gesture.getStrokesCount() + "\r\n");
+		for(Prediction p : predictions) {
+			tv.append(p.name + ": " + p.score + "\r\n");
 		}
+		
+	}
+
+	private void onMatchFound(GestureOverlayView overlay, Gesture gesture, Prediction prediction) {
+		Log.i(TAG, "Match Found!: "+prediction.name);
+		
+	}
 }
